4e7c8e88ed45e341ed48b6b3120a115c42af097d
vanesuarez <vanesuarez2301@gmail.com>
2023-11-16 18:46:36 -0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,7 +25,7 @@ const createUser = (req, res) => {
 const updateUser = (req, res) => {
   const updatedUser = userModel.updateUser(req.params.id, req.body);
   if (updatedUser) {
-    res.status(404).json(updatedUser);
+    res.status(200).json(updatedUser); // se cambio codigo de error HTTP 404 por codigo 200 de solicitud con exito
   } else {
     res.status(404).json({ message: "Usuario no encontrado" });
   }
